16e65cb61202acac5c1bedb35aba2fc677feee39
Chaitra221994 <chaitral.rpea27@gmail.com>
2024-03-07 22:40:26 +0530

    Supplier

---
+++ src/test/java/chaitra_customer_module/Supplier_Module_Test.java
@@ -0,0 +1,17 @@
+package chaitra_customer_module;
+
+import org.testng.annotations.Test;
+
+public class Supplier_Module_Test {
+	@Test(groups="smokesuite")
+	public void samplescript_3()
+	{
+		System.out.println("-----test script 3-----");
+	}
+	
+	@Test(groups={"smokesuite","regressionsuite"})
+	public void samplescript_4()
+	{
+		System.out.println("-----test script 4-----");
+	}
+}
